af01ffa3547fb51cf73679123586fe5e13806b4e
Erik Waibel <ejwaibel+github@gmail.com>
2020-04-03 11:31:03 -0700

    Load initial set of people

--- src/app/biosphere/biosphere.component.ts
+++ src/app/biosphere/biosphere.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { BiosphereService } from './biosphere.service';
 import { Person } from '../core/model/person.model';
 import { Observable } from 'rxjs';
+import { PersonService } from '../person/person.service';
 
 @Component({
   selector: 'app-biosphere',
@@ -11,11 +12,11 @@ import { Observable } from 'rxjs';
 export class BiosphereComponent implements OnInit {
   public persons$: Observable<Person[]>;
 
-  constructor(private bioService: BiosphereService) {
+  constructor(private bioService: BiosphereService, private personService: PersonService) {
     this.persons$ = bioService.persons$;
   }
 
   ngOnInit() {
+    this.personService.getAll();
   }
-
 }
